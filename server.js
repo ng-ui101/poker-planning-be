0d722220934024dd22cdf0a58056ff2debda8812
Igor <igor.letterbx@gmail.com>
2022-09-11 23:31:54 +0300

    configure server for new ws message types

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-import { WebSocketServer as Server } from 'ws';
+import {WebSocketServer as Server} from 'ws';
 import express from 'express';
 import cors from 'cors';
 import {v4 as uuid} from 'uuid';
 
 const roomClientsDict = {};
-const roomEventsDict = {};
-const estimateDataDict = {};
-const nameDataDict = {};
+const roomEstimateEvents = [];
 const activeRooms = [];
 
 const app = express();
@@ -17,11 +15,9 @@ app.post('/api/create-room', function (request, response) {
     const roomId = uuid();
 
     roomClientsDict[roomId] = {};
-    roomEventsDict[roomId] = [];
-    estimateDataDict[roomId] = {};
-    nameDataDict[roomId] = {};
+    roomEstimateEvents[roomId] = [];
     activeRooms.push(roomId);
-    
+
     response.send({roomId})
 });
 
@@ -37,91 +33,87 @@ const wss = new Server({port: 8000});
 
 wss.on('connection', (ws, req) => {
     const roomId = req.url.slice(1)
-    console.log('room is create', roomId)
+    console.log('connection to room:', roomId)
 
     const clientId = uuid();
-    let clientName = '';
-    
+
     const clients = roomClientsDict[roomId];
-    const roomEvents = roomEventsDict[roomId];
-    const estimateData = estimateDataDict[roomId];
-    const nameData = nameDataDict[roomId];
 
     clients[clientId] = ws;
 
     console.log(`New client ${clientId}`);
-    ws.send(JSON.stringify(roomEvents));
 
-    //action is {
-    // type: reset | accept | setUserName | estimate | leave
-    // data: null | null | [name] | [estimate] | null
-    //}
     ws.on('message', (rawMessage) => {
-        let {name, action} = JSON.parse(rawMessage);
+        let {type, name, estimate} = JSON.parse(rawMessage);
 
-        if (action.type === 'estimate') {
-            estimateData[clientId] = action.data;
-            
-            const amount = Object.keys(estimateData).length;
-            if (amount === Object.keys(clients).length) {
-                let summ = 0;
-                for (const est in estimateData) {
-                    summ += estimateData[est];
-                }
-                const finalEstimate = summ / amount;
-
-                for (const id in clients) {
-                    clients[id].send(JSON.stringify({finalEstimate}))
-                }
-                estimateDataDict[roomId] = {}
+        if (type === 'set-estimate') {
+            let index = roomEstimateEvents[roomId].findIndex((ev) => ev.clientId === clientId);
+            if (index >= 0) {
+                roomEstimateEvents[roomId].splice(index, 1, {clientId, name, estimate})
+            } else {
+                roomEstimateEvents[roomId].push({clientId, name, estimate});
             }
-        }
 
-        if (action.type === 'accept') {
-            const amount = Object.keys(estimateData).length;
-            let summ = 0;
-            for (const est in estimateData) {
-                summ += estimateData[est];
-            }
-            const finalEstimate = summ / amount;
             for (const id in clients) {
-                clients[id].send(JSON.stringify({finalEstimate}))
+                clients[id].send(JSON.stringify({
+                    type: 'estimates',
+                    estimates: roomEstimateEvents[roomId]
+                }))
             }
-            estimateDataDict[roomId] = {};
         }
 
-        if (action.type === 'reset') {
-            estimateDataDict[roomId] = {}
+        if (type === 'accept') {
+            const amount = roomEstimateEvents[roomId].length;
+            const sum = roomEstimateEvents[roomId].reduce((p, c) => p.estimate + c.estimate);
+            const finalEstimate = sum / amount;
+
+            for (const id in clients) {
+                clients[id].send(JSON.stringify({
+                    type: 'final-estimate',
+                    finalEstimate,
+                    estimates: roomEstimateEvents[roomId]
+                }))
+            }
+            roomEstimateEvents[roomId] = []
         }
 
-        if (action.type === 'setUserName') {
-            name = action.data;
-            clientName = action.data;
-            nameData[clientId] = action.data;
-            console.log('nameData', nameData)
+        if (type === 'reset') {
+            roomEstimateEvents[roomId] = []
+
+            for (const id in clients) {
+                clients[id].send(JSON.stringify({
+                    type: 'reset',
+                    estimates: roomEstimateEvents[roomId]
+                }))
+            }
         }
 
-        roomEvents.push({name, action});
-        
-        for (const id in clients) {
-            clients[id].send(JSON.stringify({name, action}))
+        if (type === 'set-user-name') {
+            ws.send(JSON.stringify({
+                type: 'estimates',
+                estimates: roomEstimateEvents[roomId]
+            }));
+        }
+
+        if (type === 'user-leave') {
+            roomEstimateEvents[roomId] = roomEstimateEvents[roomId].filter((r) => r.clientId !== clientId);
+
+            for (const id in clients) {
+                clients[id].send(JSON.stringify({
+                    type: 'user-leave',
+                    estimates: roomEstimateEvents[roomId]
+                }))
+            }
         }
     })
 
     ws.on('close', () => {
-        for (const id in clients) {
-            clients[id].send(JSON.stringify({name: clientName, action: {type: 'leave'}}));
-        }
-        
         delete clients[clientId];
-        delete estimateData[clientId];
         console.log(`Client is closed ${clientId}`)
-        
+
         if (Object.keys(clients).length === 0) {
             delete roomClientsDict[roomId];
-            delete roomEventsDict[roomId];
-            delete estimateDataDict[roomId];
-            delete nameDataDict[roomId];
+            delete roomEstimateEvents[roomId];
             const index = activeRooms.findIndex((r) => r === roomId);
             activeRooms.splice(index, 1);
         }
